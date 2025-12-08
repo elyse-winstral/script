3c2fc947c68c14e8649b6c7d11012ee85b372f6c
Elyse Winstral <elyseeemw@gmail.com>
2025-12-08 13:43:14 +0100

    Changed text cell

--- docs/tanks.py
+++ docs/tanks.py
@@ -1,18 +1,5 @@
 import marimo
 
-'''
-Conceptual Overview:
-
-The German tank problem is a popular estimation example in statistics. It consists of estimating the maximum of a discrete unfiform distribution by sampling without replacement. The example highlights the contrast between Frequentist and Bayesian inference. In frequentist statistics, it is assumed that the parameter of interest (the value we wish to estimate) is fixed. The underlying truth i.e. the maximum amount of tanks produced is a non-random value. In Bayesian statistics, it is assumed the maximum $N$ is random - it can take on different values and has a distribution. 
-
-The Frequentist approach estimates a single value for $\hat{N}$ - the estimation of $N$. This is given by $\hat{N} = m + \frac{m-k}{k}$. This can be seen as using the most likely estimator: the largest observed number $m$ added to the negative bias. (The bias is the expected difference between m and N- because m is a random sample the expectation can be taken w.r.t. to this distribution). 
-
-The Bayesian approach models $N$ as a random variable with a so called prior distribution, denoted $\pi(N)$. A key element of Bayesian statistics lies in determining this prior. Here we use $\pi(N) \propto N ^ {-\alpha}$. Notice if \alpha = 0, we assume $N$ has a flat distribution- any outcome is equally likely. By changing $\alpha$ > 0, we can model certain beliefs or assumptions about the distribution of $N$ (i.e. whether it is concentrated around a certain value). By taking the expectation of $N | M = m, K = k$ we can estimate the amount of produced tanks given our observations.
-
-For $\alpha = 1$ we get approximately the Frequentist formula. 
-
-'''
-
 __generated_with = "0.17.8"
 app = marimo.App(width="medium")
 
@@ -44,7 +31,7 @@ def estimate_total_tanks(max_serial: int, num_observed: int, alpha: float = 1.5)
     return ((alpha + num_observed) / (alpha + num_observed - 1)) * max_serial
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
     How can $N$ be estimated?
@@ -53,7 +40,7 @@ def _(mo):
 
     $$ \hat{N} = m + \frac{m - k}{k}  = m + \frac{m}{k} - 1 \approx \frac{1 + k}{k} m$$
 
-    However, if we imagine $N$ to be random as well, we can model $P(N = n | M = m, K = k)$. From here we could find the expectation $n$ given our observations.
+    However, if we imagine $N$ to be random as well, we can model $P(N = n | M = m, K = k)$. From here we could find the expectation of $n$ given our observations $\mathbb{E}(N | M = m, K = k)$.
     """)
     return
 
@@ -70,7 +57,6 @@ def _(np):
         observed_serials = rng.choice(range(1, monthly_production + 1), num_observed, replace=False)
         max_serial = max(observed_serials)
         estimates.append(estimate_total_tanks(max_serial, num_observed))
-
     return estimates, monthly_production
 
 
